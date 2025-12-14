f9faff2f242a1d21192573af210aa4d8c4b500a0
Vivexplorer <137091298+Vivexplorer@users.noreply.github.com>
2025-12-14 10:24:06 -0500

    Updated pricing.tsx

--- components/sections/pricing.tsx
+++ components/sections/pricing.tsx
@@ -98,7 +98,7 @@ export function Pricing() {
                 </ul>
                 <Button
 
-                  onClick={() => scrollToSection('cta')}
+                  
 
 
                   className={`w-full ${
